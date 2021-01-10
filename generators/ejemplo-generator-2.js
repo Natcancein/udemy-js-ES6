3a66c263c5d95ce5a30b6a43846ab846e2a8ecfe
Natcancein <nataliaconstanz@gmail.com>
2021-01-10 15:55:11 -0400

    agregando delegation of generators

---
+++ generators/ejemplo-generator-2.js
@@ -0,0 +1,73 @@
+const engineeringTeam=
+{
+    size:3,
+    department: 'Engineering',
+    lead:'Jill',
+    manager: 'Alex',
+    engineer: 'Dave'
+}
+
+//Creando el generador
+
+function* TeamIterator(team){
+    yield team.lead;
+    yield team.manager;
+    yield team.engineer;
+}
+
+const names =[];
+
+for (let name of TeamIterator(engineeringTeam)){
+    names.push(name);
+}
+
+console.log(names);
+
+// DELEGATION GENERATORS
+
+// agregando otro equipo
+const testingTeam={
+    lead:'Amanda',
+    tester: 'Bill'
+}
+
+const engineeringTeam=
+{
+     //agregando el equipo que esta fuera
+    //testingTeam:testingTeam;
+    //Basta con poner solo un nombre porque se repiten el key y la property
+    testingTeam,
+    size:3,
+    department: 'Engineering',
+    lead:'Jill',
+    manager: 'Alex',
+    engineer: 'Dave'
+ 
+}
+
+
+function* TeamIterator(team){
+    yield team.lead;
+    yield team.manager;
+    yield team.engineer;
+    const testingTeamGenerator = TestingTeamIterator(team.testingTeam);  
+/* agregando yield* a la constante que guarda
+el generador testingTeamIterator que tiene como parametro el integrante
+ del testing team se hace la delegacion del generador y ahora se 
+ unen ambos equipos*/
+    yield* testingTeamGenerator;
+
+}
+
+function* TestingTeamIterator(team){
+    yield team.lead;
+    yield team.tester;
+}
+
+const names =[];
+
+for (let name of TeamIterator(engineeringTeam)){
+    names.push(name);
+}
+
+console.log(names);
